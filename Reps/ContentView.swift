282c55805ed509a238d8f9b573517b75adb6f663
Aditya Rai <adzirai@amazon.com>
2024-06-22 20:11:24 -0700

    Initial Commit

---
+++ Reps/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Reps
+//
+//  Created by Rai, Adi on 6/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Reps/RepsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepsApp.swift
+//  Reps
+//
+//  Created by Rai, Adi on 6/22/24.
+//
+
+import SwiftUI
+
+@main
+struct RepsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
