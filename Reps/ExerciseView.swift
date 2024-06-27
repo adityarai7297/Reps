83a971b1c4f3d410316f21ea286e7214b80a8297
Aditya Rai <aditya.rai7297@gmail.com>
2024-06-27 00:17:07 -0700

    checkpoint 2

--- Reps/ExerciseView.swift
+++ Reps/ExerciseView.swift
@@ -17,7 +17,7 @@ struct ExerciseView: View {
                 .font(.largeTitle.bold())
             
             Rectangle()
-                .frame(height: 1)
+                .frame(height: 0.5)
                 .foregroundColor(.gray)
             
             Spacer().frame(height: 60)
