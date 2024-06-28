1ae8b3e5c8a25e0eb81359990368a7e6f7fd7ba9
Aditya Rai <aditya.rai7297@gmail.com>
2024-06-28 15:03:57 -0700

    checkpoint 3 - wheelPicker not working after adding paging

--- Reps/ContentView.swift
+++ Reps/ContentView.swift
@@ -26,95 +26,63 @@ struct ContentView: View {
     @State private var exertionValue: CGFloat = 50
     @State private var showCheckmark: Bool = false
     @State private var setCount: Int = 0
+    @State private var currentIndex: Int = 0
+    
+    var exercises: [ExerciseView] {
+        [
+            ExerciseView(
+                exerciseName: "Bench Press",
+                weightValue: $weightValue,
+                repValue: $repValue,
+                exertionValue: $exertionValue,
+                setCount: $setCount,
+                showCheckmark: $showCheckmark,
+                weightWheelConfig: weightWheelConfig,
+                repWheelConfig: repWheelConfig,
+                exertionWheelConfig: exertionWheelConfig
+            ),
+            ExerciseView(
+                exerciseName: "Squat",
+                weightValue: $weightValue,
+                repValue: $repValue,
+                exertionValue: $exertionValue,
+                setCount: $setCount,
+                showCheckmark: $showCheckmark,
+                weightWheelConfig: weightWheelConfig,
+                repWheelConfig: repWheelConfig,
+                exertionWheelConfig: exertionWheelConfig
+            ),
+            ExerciseView(
+                exerciseName: "Deadlift",
+                weightValue: $weightValue,
+                repValue: $repValue,
+                exertionValue: $exertionValue,
+                setCount: $setCount,
+                showCheckmark: $showCheckmark,
+                weightWheelConfig: weightWheelConfig,
+                repWheelConfig: repWheelConfig,
+                exertionWheelConfig: exertionWheelConfig
+            ),
+            ExerciseView(
+                exerciseName: "Bicep Curl",
+                weightValue: $weightValue,
+                repValue: $repValue,
+                exertionValue: $exertionValue,
+                setCount: $setCount,
+                showCheckmark: $showCheckmark,
+                weightWheelConfig: weightWheelConfig,
+                repWheelConfig: repWheelConfig,
+                exertionWheelConfig: exertionWheelConfig
+            )
+        ]
+    }
     
     var body: some View {
-        NavigationStack {
-            VStack {
-                VStack {
-                    Text("Bench Press")
-                        .font(.largeTitle.bold())
-                
-                    Rectangle()
-                        .frame(height: 0.5)
-                        .foregroundColor(.gray)
-                    
-                    Spacer().frame(height: 60)
-                    
-                    HStack(alignment: .lastTextBaseline, spacing: 5, content: {
-                        Text(verbatim: "\(weightValue)")
-                            .font(.largeTitle.bold())
-                            .contentTransition(.numericText(value: weightValue))
-                            .animation(.snappy, value: weightValue)
-                        
-                        Text("lbs")
-                            .font(.title2)
-                            .fontWeight(.semibold)
-                            .textScale(.secondary)
-                            .foregroundStyle(.gray)
-                    })
-                    
-                    WheelPicker(config: weightWheelConfig, value: $weightValue)
-                        .frame(height: 60)
-                    
-                    Spacer().frame(height: 40)
-                    
-                    HStack(alignment: .lastTextBaseline, spacing: 5, content: {
-                        Text(verbatim: "\(Int(repValue))")
-                            .font(.largeTitle.bold())
-                            .contentTransition(.numericText(value: repValue))
-                            .animation(.snappy, value: repValue)
-                        
-                        Text("reps")
-                            .font(.title2)
-                            .fontWeight(.semibold)
-                            .textScale(.secondary)
-                            .foregroundStyle(.gray)
-                    })
-                    
-                    WheelPicker(config: repWheelConfig, value: $repValue)
-                        .frame(height: 60)
-                    
-                    Spacer().frame(height: 40)
-                    
-                    HStack(alignment: .lastTextBaseline, spacing: 5, content: {
-                        Text(verbatim: "\(Int(exertionValue))")
-                            .font(.largeTitle.bold())
-                            .contentTransition(.numericText(value: exertionValue))
-                            .animation(.snappy, value: exertionValue)
-                        
-                        Text("%  RPE")
-                            .font(.title2)
-                            .fontWeight(.semibold)
-                            .textScale(.secondary)
-                            .foregroundStyle(.gray)
-                    })
-                    
-                    WheelPicker(config: exertionWheelConfig, value: $exertionValue)
-                        .frame(height: 60)
-                }
-                .offset(y: showCheckmark ? -60 : 0) // Adjust offset value to move elements up
-                
-                Spacer().frame(height: 40)
-                
-                HStack{
-                    Spacer().frame(width: 12)
-                    SetButton(showCheckmark: $showCheckmark, setCount: $setCount, action: {
-                        // this would be replaced by stateful operations
-                        setCount += 1
-                        print("Weight: \(weightValue) lbs")
-                        print("Reps: \(repValue)")
-                        print("Exertion: \(exertionValue) % RPE")
-                    })
-                    Spacer().frame(width: 12)
-                    Text("\(setCount)")
-                        .font(.title)
-                        .opacity(setCount > 0 ? 1 : 0)
-                    
-                }
-                
+        VerticalPager(pageCount: exercises.count, currentIndex: $currentIndex) {
+            ForEach(0..<exercises.count, id: \.self) { index in
+                exercises[index]
             }
         }
-        .navigationTitle("Wheel Picker")
     }
 }
 
--- Reps/View/VerticalPager.swift
+++ Reps/View/VerticalPager.swift
@@ -1,8 +1,47 @@
-//
-//  VerticalPager.swift
-//  Reps
-//
-//  Created by Rai, Adi on 6/28/24.
-//
+import SwiftUI
 
-import Foundation
+struct VerticalPager<Content: View>: View {
+    let pageCount: Int
+    @Binding var currentIndex: Int
+    let content: () -> Content
+    
+    init(pageCount: Int, currentIndex: Binding<Int>, @ViewBuilder content: @escaping () -> Content) {
+        self.pageCount = pageCount
+        self._currentIndex = currentIndex
+        self.content = content
+    }
+    
+    @GestureState private var translation: CGFloat = 0
+    
+    var body: some View {
+        GeometryReader { geometry in
+            LazyVStack(spacing: 0) {
+                self.content().frame(width: geometry.size.width, height: geometry.size.height)
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.primary.opacity(0.000000001))
+            .offset(y: -CGFloat(self.currentIndex) * geometry.size.height)
+            .offset(y: self.translation)
+            .animation(.interactiveSpring(response: 0.3), value: currentIndex)
+            .animation(.interactiveSpring(), value: translation)
+            .gesture(
+                DragGesture(minimumDistance: 1).updating(self.$translation) { value, state, _ in
+                    state = value.translation.height
+                }.onEnded { value in
+                    let offset = -Int(value.translation.height)
+                    if abs(offset) > Int(geometry.size.height / 2) || abs(value.velocity.height) > 200 {
+                        let newIndex = currentIndex + min(max(offset, -1), 1)
+                        if newIndex >= 0 && newIndex < pageCount {
+                            self.currentIndex = newIndex
+                        }
+                    }
+                }
+            )
+        }
+        .edgesIgnoringSafeArea(.all) // Ensure the pager view occupies the entire screen area
+    }
+}
+
+#Preview {
+    ContentView()
+}
