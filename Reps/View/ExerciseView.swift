111f577f182153b8ff1079f3b67a2b010a5e6e60
Aditya Rai <aditya.rai7297@gmail.com>
2024-06-28 18:27:50 -0700

    CHECKPOINT!! gestures working!! :tada: :tada:

--- Reps/View/ExerciseView.swift
+++ Reps/View/ExerciseView.swift
@@ -18,62 +18,74 @@ struct ExerciseView: View {
                     .font(.largeTitle.bold())
                 
                 Rectangle()
-                    .frame(height: 0.5)
+                    .frame(height: 0.3)
                     .foregroundColor(.gray)
                 
                 Spacer().frame(height: 60)
                 
-                HStack(alignment: .lastTextBaseline, spacing: 5, content: {
-                    Text(verbatim: "\(weightValue)")
-                        .font(.largeTitle.bold())
-                        .contentTransition(.numericText(value: weightValue))
-                        .animation(.snappy, value: weightValue)
+                VStack{
+                    HStack(alignment: .lastTextBaseline, spacing: 5, content: {
+                        Text(verbatim: "\(weightValue)")
+                            .font(.largeTitle.bold())
+                            .contentTransition(.numericText(value: weightValue))
+                            .animation(.snappy, value: weightValue)
+                        
+                        Text("lbs")
+                            .font(.title2)
+                            .fontWeight(.semibold)
+                            .textScale(.secondary)
+                            .foregroundStyle(.gray)
+                    })
                     
-                    Text("lbs")
-                        .font(.title2)
-                        .fontWeight(.semibold)
-                        .textScale(.secondary)
-                        .foregroundStyle(.gray)
-                })
+                    WheelPicker(config: weightWheelConfig, value: $weightValue)
+                        .frame(height: 60)
+                    
+                }
                 
-                WheelPicker(config: weightWheelConfig, value: $weightValue)
-                    .frame(height: 60)
                 
                 Spacer().frame(height: 40)
                 
-                HStack(alignment: .lastTextBaseline, spacing: 5, content: {
-                    Text(verbatim: "\(Int(repValue))")
-                        .font(.largeTitle.bold())
-                        .contentTransition(.numericText(value: repValue))
-                        .animation(.snappy, value: repValue)
+                VStack{
+                    HStack(alignment: .lastTextBaseline, spacing: 5, content: {
+                        Text(verbatim: "\(Int(repValue))")
+                            .font(.largeTitle.bold())
+                            .contentTransition(.numericText(value: repValue))
+                            .animation(.snappy, value: repValue)
+                        
+                        Text("reps")
+                            .font(.title2)
+                            .fontWeight(.semibold)
+                            .textScale(.secondary)
+                            .foregroundStyle(.gray)
+                    })
                     
-                    Text("reps")
-                        .font(.title2)
-                        .fontWeight(.semibold)
-                        .textScale(.secondary)
-                        .foregroundStyle(.gray)
-                })
+                    WheelPicker(config: repWheelConfig, value: $repValue)
+                        .frame(height: 60)
+                    
+                }
                 
-                WheelPicker(config: repWheelConfig, value: $repValue)
-                    .frame(height: 60)
                 
                 Spacer().frame(height: 40)
                 
-                HStack(alignment: .lastTextBaseline, spacing: 5, content: {
-                    Text(verbatim: "\(Int(exertionValue))")
-                        .font(.largeTitle.bold())
-                        .contentTransition(.numericText(value: exertionValue))
-                        .animation(.snappy, value: exertionValue)
+                VStack{
+                    HStack(alignment: .lastTextBaseline, spacing: 5, content: {
+                        Text(verbatim: "\(Int(exertionValue))")
+                            .font(.largeTitle.bold())
+                            .contentTransition(.numericText(value: exertionValue))
+                            .animation(.snappy, value: exertionValue)
+                        
+                        Text("%  RPE")
+                            .font(.title2)
+                            .fontWeight(.semibold)
+                            .textScale(.secondary)
+                            .foregroundStyle(.gray)
+                    })
                     
-                    Text("%  RPE")
-                        .font(.title2)
-                        .fontWeight(.semibold)
-                        .textScale(.secondary)
-                        .foregroundStyle(.gray)
-                })
+                    WheelPicker(config: exertionWheelConfig, value: $exertionValue)
+                        .frame(height: 60)
+                    
+                }
                 
-                WheelPicker(config: exertionWheelConfig, value: $exertionValue)
-                    .frame(height: 60)
             }
             .offset(y: showCheckmark ? -60 : 0) // Adjust offset value to move elements up
             
