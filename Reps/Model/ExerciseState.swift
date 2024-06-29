53de6b1e05ce328a734ebd38c781df6c362a619c
Aditya Rai <aditya.rai7297@gmail.com>
2024-06-28 18:41:42 -0700

    State management cleanup

---
+++ Reps/Model/ExerciseState.swift
@@ -0,0 +1,9 @@
+import SwiftUI
+
+struct ExerciseState {
+    var weightValue: CGFloat
+    var repValue: CGFloat
+    var exertionValue: CGFloat
+    var setCount: Int
+    var showCheckmark: Bool
+}
