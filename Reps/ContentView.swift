5c9dfae6a2239814ac6e575c09ab2f802575649f
Aditya Rai <aditya.rai7297@gmail.com>
2024-07-04 01:56:26 -0700

    fix

--- Reps/ContentView.swift
+++ Reps/ContentView.swift
@@ -2,25 +2,9 @@ import SwiftUI
 import FirebaseFirestore
 
 struct ContentView: View {
-    @State private var weightWheelConfig: WheelPicker.Config = .init(
-        count: 100,
-        steps: 10,
-        spacing: 7,
-        multiplier: 5
-    )
-    @State private var repWheelConfig: WheelPicker.Config = .init(
-        count: 100,
-        steps: 1,
-        spacing: 50,
-        multiplier: 1
-    )
-    
-    @State private var exertionWheelConfig: WheelPicker.Config = .init(
-        count: 10,
-        steps: 1,
-        spacing: 50,
-        multiplier: 10
-    )
+    @State private var weightWheelConfig: WheelPicker.Config = .init(count: 100, steps: 10, spacing: 7, multiplier: 5)
+    @State private var repWheelConfig: WheelPicker.Config = .init(count: 100, steps: 1, spacing: 50, multiplier: 1)
+    @State private var exertionWheelConfig: WheelPicker.Config = .init(count: 10, steps: 1, spacing: 50, multiplier: 10)
     
     @State private var exerciseStates: [ExerciseState] = []
     @State private var currentIndex: Int = 0
@@ -37,11 +21,7 @@ struct ContentView: View {
                         loadExercisesFromFirebase()
                     }
             } else if exerciseStates.isEmpty {
-                VStack {
-                    // add a cute image here
-                    Text("Start adding exercises!")
-                    
-                }
+                EmptyStateView()
             } else {
                 VerticalPager(pageCount: exerciseStates.count, currentIndex: $currentIndex) {
                     ForEach(exerciseStates.indices, id: \.self) { index in
@@ -95,7 +75,7 @@ struct ContentView: View {
             }
         }
         .sheet(isPresented: $showExerciseListView) {
-            ExerciseListView(exerciseStates: $exerciseStates, userId: userId, startBouncingArrow: startBouncingArrow)
+            ExerciseListView(exerciseStates: $exerciseStates, currentIndex: $currentIndex, userId: userId)
         }
     }
 
@@ -122,10 +102,14 @@ struct ContentView: View {
                 }
                 print("Loaded exercises: \(self.exerciseStates)")
                 self.isLoading = false
+                // Ensure currentIndex is within bounds
+                if currentIndex >= exerciseStates.count {
+                    currentIndex = max(0, exerciseStates.count - 1)
+                }
             }
         }
     }
-    
+
     func loadCurrentState(for exerciseName: String, at index: Int) {
         let db = Firestore.firestore()
         let userRef = db.collection("users").document(userId)
@@ -156,120 +140,8 @@ struct ContentView: View {
     }
 }
 
-struct ExerciseListView: View {
-    @Binding var exerciseStates: [ExerciseState]
-    @State private var newExerciseName: String = ""
-    @Environment(\.presentationMode) var presentationMode
-    let userId: String
-    let startBouncingArrow: () -> Void
-    
-    var body: some View {
-        NavigationView {
-            VStack {
-                Form {
-                    Section(header: Text("Add New Exercise")) {
-                        TextField("Exercise Name", text: $newExerciseName)
-                        Button("Add Exercise") {
-                            if !newExerciseName.isEmpty {
-                                let newExercise = ExerciseState(
-                                    exerciseName: newExerciseName,
-                                    lastWeightValue: 0,
-                                    lastRepValue: 0,
-                                    lastRPEValue: 0,
-                                    setCount: 0,
-                                    showCheckmark: false
-                                )
-                                exerciseStates.append(newExercise)
-                                addExerciseToFirebase(userId: userId, exerciseName: newExerciseName)
-                                newExerciseName = ""
-                            }
-                        }
-                    }
-                    
-                    Section(header: Text("My Exercises")) {
-                        ForEach(exerciseStates.indices, id: \.self) { index in
-                            HStack {
-                                Text(exerciseStates[index].exerciseName)
-                                Spacer()
-                                Button(action: {
-                                    removeExerciseFromFirebase(userId: userId, exerciseName: exerciseStates[index].exerciseName)
-                                    exerciseStates.remove(at: index)
-                                    startBouncingArrow() // Restart animation after deletion
-                                }) {
-                                    Image(systemName: "trash")
-                                        .foregroundColor(.red)
-                                }
-                                .buttonStyle(BorderlessButtonStyle()) // Ensure only the icon is tappable
-                                
-                                Image(systemName: "line.horizontal.3")
-                                    .padding(.leading)
-                                
-                            }
-                        }
-                        .onMove(perform: move)
-                    }
-                }
-                .navigationTitle("Manage Exercises")
-                .navigationBarItems(trailing: Button("Done") {
-                    presentationMode.wrappedValue.dismiss()
-                })
-            }
-            .preferredColorScheme(.dark) // Force dark mode only here
-        }
-    }
-    
-    func move(from source: IndexSet, to destination: Int) {
-        exerciseStates.move(fromOffsets: source, toOffset: destination)
-    }
-    
-    func addExerciseToFirebase(userId: String, exerciseName: String) {
-        let db = Firestore.firestore()
-        let userRef = db.collection("users").document(userId)
-        userRef.collection("exercises").document(exerciseName).setData([
-            "exerciseName": exerciseName
-        ]) {
-            error in
-            if let error = error {
-                print("Error adding document: (error)")
-            } else {
-                print("Document added successfully")
-            }
-        }
-    }
-    func removeExerciseFromFirebase(userId: String, exerciseName: String) {
-        let db = Firestore.firestore()
-        let userRef = db.collection("users").document(userId)
-        userRef.collection("exercises").document(exerciseName).delete { error in
-            if let error = error {
-                print("Error removing document: \(error)")
-            } else {
-                print("Document removed successfully")
-                if let index = exerciseStates.firstIndex(where: { $0.exerciseName == exerciseName }) {
-                                exerciseStates.remove(at: index)
-                            }
-            }
-        }
-    }
-    
-}
-
-extension Color {
-    init(hex: String) {
-        let scanner = Scanner(string: hex)
-        scanner.currentIndex = hex.startIndex
-        
-        var rgbValue: UInt64 = 0
-        scanner.scanHexInt64(&rgbValue)
-        
-        let red = Double((rgbValue & 0xff0000) >> 16) / 255.0
-        let green = Double((rgbValue & 0x00ff00) >> 8) / 255.0
-        let blue = Double(rgbValue & 0x0000ff) / 255.0
-        
-        self.init(red: red, green: green, blue: blue)
-    }
-}
-    
-    #Preview {
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
         ContentView()
     }
-
+}
