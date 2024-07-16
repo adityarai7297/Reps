0ad9e1f1eb9c69036687a1032363c7fc81f72668
Aditya Rai <aditya.rai7297@gmail.com>
2024-07-16 02:02:16 -0700

    blur menu effect

--- Reps/View/MenuView.swift
+++ Reps/View/MenuView.swift
@@ -3,38 +3,33 @@ import SwiftUI
 struct MenuView: View {
     @Binding var showExerciseListView: Bool
     @Binding var userId: String
-    @Binding var exerciseStates:  [ExerciseState]
+    @Binding var exerciseStates: [ExerciseState]
     @Binding var currentIndex: Int
+
     var body: some View {
         VStack {
             Button(action: {
-                                   showExerciseListView = true
-                               }) {
-                Text("Manage Exercises")
-                    .foregroundColor(.black)
-                    .padding()
-            }
-            Button(action: {
-                print("Button 2 clicked")
+                // Action for the button
             }) {
-                Text("Button 2")
-                    .foregroundColor(.black)
-                    .padding()
+                Text("Menu Item 1")
             }
+            .padding()
+            .background(Color.clear)
+            .foregroundColor(Color.black)
+
             Button(action: {
-                print("Button 3 clicked")
+                // Action for the button
             }) {
-                Text("Button 3")
-                    .foregroundColor(.black)
-                    .padding()
+                Text("Menu Item 2")
             }
+            .padding()
+            .background(Color.clear)
+            .foregroundColor(Color.black)
+
+            // Add more menu items here
+
+            Spacer()
         }
-        .background(Color.white)
-        .cornerRadius(10)
-        .shadow(radius: 5)
         .padding()
-        .sheet(isPresented: $showExerciseListView) {
-                   ExerciseListView(exerciseStates: $exerciseStates, currentIndex: $currentIndex, userId: userId)
-               }
     }
 }
