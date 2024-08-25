c5ed06b4bf031d1e853c4f3f615ec490a9d06c5d
Aditya Rai <aditya.rai7297@gmail.com>
2024-08-24 23:32:29 -0700

    swiftdata

--- Reps/ContentView.swift
+++ Reps/ContentView.swift
@@ -5,15 +5,30 @@ struct ContentView: View {
     @State private var weightWheelConfig: WheelPicker.Config = .init(count: 100, steps: 10, spacing: 7, multiplier: 5)
     @State private var repWheelConfig: WheelPicker.Config = .init(count: 100, steps: 1, spacing: 50, multiplier: 1)
     @State private var exertionWheelConfig: WheelPicker.Config = .init(count: 10, steps: 1, spacing: 50, multiplier: 10)
-    @State private var exerciseStates: [ExerciseState] = [
-        ExerciseState(exerciseName: "Bench Press", lastWeightValue: 135.0, lastRepValue: 8, lastRPEValue: 7, setCount: 3, showCheckmark: false),
-        ExerciseState(exerciseName: "Squat", lastWeightValue: 185.0, lastRepValue: 6, lastRPEValue: 8, setCount: 4, showCheckmark: false),
-        ExerciseState(exerciseName: "Deadlift", lastWeightValue: 225.0, lastRepValue: 5, lastRPEValue: 9, setCount: 3, showCheckmark: false),
-        ExerciseState(exerciseName: "Overhead Press", lastWeightValue: 95.0, lastRepValue: 10, lastRPEValue: 6, setCount: 3, showCheckmark: false),
-        ExerciseState(exerciseName: "Barbell Row", lastWeightValue: 115.0, lastRepValue: 8, lastRPEValue: 7, setCount: 4, showCheckmark: false)
-    ]
     @State private var userId = "XXXXX"
     @State private var currentIndex: Int = 0
+    @State private var exerciseStates: [ExerciseState] = [
+        ExerciseState(exerciseName: "Bench Press", 
+                      lastWeightValue: 135.0,
+                      lastRepValue: 8,
+                      lastRPEValue: 7,
+                      setCount: 3,
+                      showCheckmark: false),
+        
+        ExerciseState(exerciseName: "Squat",
+                      lastWeightValue: 185.0,
+                      lastRepValue: 6, lastRPEValue: 8,
+                      setCount: 4,
+                      showCheckmark: false),
+        
+        ExerciseState(exerciseName: "Deadlift",
+                      lastWeightValue: 225.0,
+                      lastRepValue: 5,
+                      lastRPEValue: 9,
+                      setCount: 3,
+                      showCheckmark: false),
+    ]
+    
 
     var body: some View {
         ZStack {
--- Reps/Model/ExerciseState.swift
+++ Reps/Model/ExerciseState.swift
@@ -1,10 +1,21 @@
+import SwiftData
 import SwiftUI
 
-struct ExerciseState {
+@Model
+final class ExerciseState {
     var exerciseName: String
     var lastWeightValue: CGFloat
     var lastRepValue: CGFloat
     var lastRPEValue: CGFloat
     var setCount: Int
     var showCheckmark: Bool
+    
+    init(exerciseName: String, lastWeightValue: CGFloat, lastRepValue: CGFloat, lastRPEValue: CGFloat, setCount: Int, showCheckmark: Bool) {
+        self.exerciseName = exerciseName
+        self.lastWeightValue = lastWeightValue
+        self.lastRepValue = lastRepValue
+        self.lastRPEValue = lastRPEValue
+        self.setCount = setCount
+        self.showCheckmark = showCheckmark
+    }
 }
