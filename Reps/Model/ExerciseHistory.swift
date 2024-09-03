32ed11fe5beb4b3424a87155b3c23875b7c48b5d
Aditya Rai <aditya.rai7297@gmail.com>
2024-09-03 12:51:21 -0700

    Deletion of exercise history achieved

--- Reps/Model/ExerciseHistory.swift
+++ Reps/Model/ExerciseHistory.swift
@@ -3,14 +3,15 @@ import SwiftUI
 
 @Model
 final class ExerciseHistory {
-    var id = UUID()
-    var exercise: Exercise // Relationship to the Exercise entity
+    @Attribute(.unique) var id: UUID
+    var exercise: Exercise
     var weight: CGFloat
     var reps: CGFloat
     var rpe: CGFloat
     var timestamp: Date
 
     init(exercise: Exercise, weight: CGFloat, reps: CGFloat, rpe: CGFloat, timestamp: Date = Date()) {
+        self.id = UUID() // Ensure each instance gets a unique UUID
         self.exercise = exercise
         self.weight = weight
         self.reps = reps
