4f7b9eef2a4c39c8477a64f3b47edda640bc9fa4
Aditya Rai <aditya.rai7297@gmail.com>
2024-10-04 12:44:19 +0530

    Working changes

--- Reps/Model/ExerciseHistory.swift
+++ Reps/Model/ExerciseHistory.swift
@@ -4,15 +4,16 @@ import SwiftUI
 @Model
 final class ExerciseHistory: ObservableObject, Identifiable {
     @Attribute(.unique) var id: UUID
-    var exerciseName: String // Store exercise name directly
+    var exerciseName: String
     var weight: CGFloat
     var reps: CGFloat
     var rpe: CGFloat
     var timestamp: Date
 
+    // Initialization
     init(exerciseName: String, weight: CGFloat, reps: CGFloat, rpe: CGFloat, timestamp: Date = Date()) {
-        self.id = UUID() // Ensure each instance gets a unique UUID
-        self.exerciseName = exerciseName // Save the exercise name directly
+        self.id = UUID()
+        self.exerciseName = exerciseName
         self.weight = weight
         self.reps = reps
         self.rpe = rpe
